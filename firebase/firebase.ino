059334e837101a94a16def54f32e583173830c09
Pi BOTS MakerHub <62341683+pibotsmakerhub@users.noreply.github.com>
2020-04-30 18:27:36 +0530

    Add files via upload

---
+++ firebase/firebase.ino
@@ -0,0 +1,131 @@
+﻿/*
+   Created by Pi BOTS MakerHub
+
+   Email: pibotsmakerhub@gmail.com
+
+   Github: https://github.com/pibotsmakerhub
+
+   Copyright (c) 2020 Pi BOTS MakerHub
+*/
+
+
+//FirebaseESP8266.h must be included before ESP8266WiFi.h
+#include "FirebaseESP8266.h"	// Install Firebase ESP8266 library
+#include <ESP8266WiFi.h>
+#include <DHT.h>		// Install DHT11 Library and Adafruit Unified Sensor Library
+
+
+#define FIREBASE_HOST "YOUR_FIREBASE_PROJECT.firebaseio.com" //Without http:// or https:// schemes
+#define FIREBASE_AUTH "YOUR_FIREBASE_DATABASE_SECRET"
+#define WIFI_SSID "YOUR_WIFI_AP"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+#define DHTPIN 2		// Connect Data pin of DHT to D2
+int led = D5;			// Connect LED to D5
+
+#define DHTTYPE    DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+//Define FirebaseESP8266 data object
+FirebaseData firebaseData;
+FirebaseData ledData;
+
+FirebaseJson json;
+
+
+void setup()
+{
+
+  Serial.begin(9600);
+
+  dht.begin();
+  pinMode(led,OUTPUT);
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+}
+
+void sensorUpdate(){
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C  ,"));
+  Serial.print(f);
+  Serial.println(F("°F  "));
+
+  if (Firebase.setFloat(firebaseData, "/FirebaseIOT/temperature", t))
+  {
+    Serial.println("PASSED");
+    Serial.println("PATH: " + firebaseData.dataPath());
+    Serial.println("TYPE: " + firebaseData.dataType());
+    Serial.println("ETag: " + firebaseData.ETag());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+
+  if (Firebase.setFloat(firebaseData, "/FirebaseIOT/humidity", h))
+  {
+    Serial.println("PASSED");
+    Serial.println("PATH: " + firebaseData.dataPath());
+    Serial.println("TYPE: " + firebaseData.dataType());
+    Serial.println("ETag: " + firebaseData.ETag());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+}
+void loop() {
+  sensorUpdate();
+  
+  if (Firebase.getString(ledData, "/FirebaseIOT/led")){
+    Serial.println(ledData.stringData());
+    if (ledData.stringData() == "1") {
+    digitalWrite(led, HIGH);
+    }
+  else if (ledData.stringData() == "0"){
+    digitalWrite(led, LOW);
+    }
+  }
+  delay(100);
+}
